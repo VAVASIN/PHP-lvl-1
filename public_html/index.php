2b333739a424383224812d93cd3b6361fd979348
GF <gf@localhost.localdomain>
2020-05-09 02:52:45 -0500

    1st day. Workspace

---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, World!";
+            print_r('My perviy scen')
+        ?>
+    </body>
+</html>
